ecd8b671b04eb707d64d1c8360c785e1156d273a
haxislancelot <78441034+haxiswillow@users.noreply.github.com>
2021-09-20 10:07:42 -0300

    Create service.sh

---
+++ service.sh
@@ -0,0 +1,9 @@
+#!/system/bin/sh
+# Apollon Plus by haxis (haxislancelot @ GitHub), if using any code from this module, keep the credits.
+MODDIR=${0%/*}
+
+# Delay before running the main script.
+sleep 35
+
+# Setup Tweaks.
+apollontweaks
